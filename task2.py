4fa9d1763a6d02fc994de147fea16878cc4c269d
natalieQoursha <96956404+natalieQoursha@users.noreply.github.com>
2022-07-14 07:49:57 +0300

    Add files via upload

--- task2.py
+++ task2.py
@@ -1,13 +1,26 @@
-import pandas as pd
-import sqlite3
-conn=sqlite3.connect("db1.db")
-cursorObject=conn.cursor()
-cursorObject.execute("CREATE TABLE imgfg(id string, img blob)")
-conn.commit()
+import cv2 as cv
+import numpy as np
 
-im=open('/Users/natalieqoursha/Documents/images//img1.png','rb').read()
+img2= cv.imread('img2.png')
+img=cv.imread('img.png')
+img3=cv.imread('img3.png')
+test1=cv.imread('testing1.png')
+test2=cv.imread('testing2.png')
 
-conn.commit()
-table = pd.read_sql_query("SELECT * FROM imgfg", conn)
-table.to_csv("imgfg" + '.csv', index_label='index')
-print(table)
+
+kernel = np.ones((5,5),np.uint8)
+erosion = cv.erode(img2,kernel,iterations = 1)
+dilation = cv.dilate(img2,kernel,iterations = 1)
+opening = cv.morphologyEx(img2, cv.MORPH_OPEN, kernel)
+closing = cv.morphologyEx(img, cv.MORPH_CLOSE, kernel)
+gradient = cv.morphologyEx(img3, cv.MORPH_GRADIENT, kernel)
+tophat = cv.morphologyEx(test1, cv.MORPH_TOPHAT, kernel)
+blackhat = cv.morphologyEx(test1, cv.MORPH_BLACKHAT, kernel)
+
+
+verti = np.concatenate((test1, blackhat), axis=1)
+
+cv.imshow("image",verti)
+cv.waitKey(7000)
+cv.destroyAllWindows()
+
